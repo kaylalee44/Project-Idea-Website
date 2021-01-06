fc4a6b222fbbd66735b4ff4be83ed86621279a8b
Kayla Lee <45502276+kaylalee44@users.noreply.github.com>
2021-01-05 18:44:57 -0800

    First draft of html

--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+function NavBar(props) {
+    return(
+        <nav>
+            
+        </nav>
+    );
+}
+
+function GenerateProjectIdeas() {
+    // code to get different project ideas
+    return(
+        <div>
+            {/* project ideas */}
+        </div>
+    );
+}
